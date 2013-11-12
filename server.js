2fa56ff8901771c08e28603917147d7c91c87457
pipsiscool <stevenluppino@comcast.net>
2013-11-12 18:23:48 -0500

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res){
-  res.send('hello world');
+  res.json("MyFile.json");
 });
 
 var port = process.env.PORT || 5000;
